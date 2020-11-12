0076ad81536be7235704fa7f993cbd6d7e76a735
quynt <quynt6803@seta-international.vn>
2020-11-12 17:25:10 +0700

    today challenge

---
+++ is_wheel.py
@@ -0,0 +1,33 @@
+# https://app.codesignal.com/challenge/CZBN32YvXfKqwX9h6
+
+
+def isWheel(adj):
+    result = list()
+    for x, _ in enumerate(adj):
+        result.append(0)
+        for y, _ in enumerate(adj):
+            if adj[x][y] and x == y:
+                return False
+            if adj[x][y]:
+                result[x] += 1
+    count3 = 0
+    count_length = 0
+    for _, item in enumerate(result):
+        if item == 3:
+            count3 += 1
+        elif item == len(adj) - 1:
+            count_length += 1
+    if (count3 == len(adj) - 1 and count_length == 1) or (count3 == len(adj) and len(adj) == 4):
+        return True
+
+    return False
+
+
+if __name__ == '__main__':
+    print(isWheel(
+        adj=[[False, True, True, True, True],
+             [True, False, True, False, True],
+             [True, True, False, True, False],
+             [True, False, True, False, True],
+             [True, True, False, True, False]])
+    )
